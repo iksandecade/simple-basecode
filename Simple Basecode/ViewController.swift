f3be0d6e14aec3e3b68204a06a068949684098b4
MCOMM00008 <>
2021-11-02 10:24:08 +0700

    first commit

---
+++ Simple Basecode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple Basecode
+//
+//  Created by MCOMM00008 on 02/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
